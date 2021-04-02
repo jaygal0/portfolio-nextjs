047a56046450d06bcb2c9e3e54ca606474b60f4c
josh <joshuagalinato@gmail.com>
2021-04-02 11:04:40 +0200

    updated now

--- components/NowDetail.js
+++ components/NowDetail.js
@@ -3,10 +3,10 @@ import React from 'react'
 const NowDetail = () => {
   return (
     <>
-      <h5>moving apartments</h5>
+      <h5>settling in</h5>
       <p>
-        I'm currently going through the slow process of packing up boxes,
-        transporting boxes, and then unpacking boxes.
+        I've finally settled into the new apartment. Now I just need to settle
+        into the new area.
       </p>
     </>
   )
